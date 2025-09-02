a8d887aa0b7b9a14cbee89a9d3cddcb760565515
maroofirfan <maroofirfan@a2kitsolutions.com>
2025-09-02 16:17:21 +0530

    version_label: "4.3"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Updates-02092025-version: 4.2
+          Test Updates-02092025-version: 4.3
         </p>
         <a
           className="App-link"
