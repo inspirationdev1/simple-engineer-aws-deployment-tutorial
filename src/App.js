9bc06cdf5c20078c4295af4c870ffde22fe7b7ee
inspirationdev1 <inspirationdev1@gmail.com>
2025-09-29 19:10:27 +0530

    Included Api Fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+  
+  const [notesListings, setNotesListings] = useState([]);
+  const [notesListingsCount, setNotesListingsCount] = useState(0);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        
+        const res = await fetch('http://notes-lb-313941488.ap-south-1.elb.amazonaws.com/notes');
+        const data = await res.json();
+        setNotesListings(data.notes);
+        setNotesListingsCount(data.notes.length); // ✅ Count from array length
+        console.log("notesListings count : " + data.notes.length);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNotes();
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Test Updates-02092025-version: 4.5
+          Listing Count : {notesListingsCount}
+          
         </p>
         <a
           className="App-link"
